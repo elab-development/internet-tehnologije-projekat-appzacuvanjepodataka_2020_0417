b36d8ea998cc79252a17b560c0e685c78def2d66
lexaue <aleksa.corbic@gmail.com>
2024-09-09 22:20:12 +0200

    Pocetak fronta, update lokacije kontrolera, brisanje welcom.blade
    Dodavanje stranice homepage,  lokacija kontrolera promenjena i izbrisan default welcome blade koji je sluzio za bekend

---
+++ ShareFiles/resources/js/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center mb-4">Welcome!</h1>
+            <p>
+                We're delighted to have you here. At ShareFiles, 
+                our goal is to provide you with exceptional services  
+                and a seamless experience. Whether you're exploring our offerings, 
+                learning more about us, or seeking support, we're here to help every step of the way.
+            </p>    
+            <p>
+                Thank you for visiting our website. We hope you find everything you're looking for, 
+                and if you have any questions or need assistance, please don't hesitate to reach out. 
+                Enjoy your visit!
+            </p>
+        </div>
+    );
+};
+
+export default HomePage;
