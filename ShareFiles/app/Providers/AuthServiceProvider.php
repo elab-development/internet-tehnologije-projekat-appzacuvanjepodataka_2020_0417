f18225273db0b7ca6169c620688609b26aa66908
lexaue <aleksa.corbic@gmail.com>
2024-09-09 11:52:16 +0200

    Registrovanje polise u AuthServiceProvider
    Registrovanje ovog pasosa

--- ShareFiles/app/Providers/AuthServiceProvider.php
+++ ShareFiles/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+
+use App\Models\Company;
+use App\Policies\CompanyPolicy;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Company::class => CompanyPolicy::class,
     ];
 
     /**
@@ -21,6 +25,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
     }
 }
