f7548c1a9dba18017e87ea3c4f36293c0021371b
lexaue <aleksa.corbic@gmail.com>
2024-09-09 22:34:38 +0200

    app.jsx

---
+++ ShareFiles/resources/js/app.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import DashboardPage from './pages/DashboardPage';
+import CompanyPage from './pages/CompanyPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
+                    <Route path="/dashboard/:companyName" element={<CompanyPage />} />  
+                    <Route path="/login" element={<LoginPage />} />
+                    {/* Rute za 404 page */}
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
+
